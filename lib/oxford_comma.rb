634c28e11b2db7326ad25827ce9520c54de00e13
tchartchke <chartai.kl@gmail.com>
2020-09-29 17:28:44 -0500

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  if array.size == 1
+    return array[0]
+  elsif array.size < 3
+    return array.join(" and ")
+  else
+    item = array.pop
+    return array.join(", ") + ", and #{item}"
+  end
 end
